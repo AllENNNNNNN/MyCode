43defad26456f9e6528c5581707c957c6db3e771
Wuchenwcf <wangcf@wangsu.com>
2017-07-24 12:52:23 +0800

    字符加密

---
+++ C++/算法/字符加密/源.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<cctype>
+using namespace std;
+
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	string key;
+	string data;
+	while (cin>>key>>data)
+	{
+		key += "abcdefghijklmnopqrstuvwxyz";
+		string temp;
+		for (size_t i = 0; i < key.size(); i++)
+		{
+			if (temp.find(key[i])==string::npos)
+			{
+				temp.push_back(key[i]);
+			}
+		}
+		
+		//cout << temp << endl;
+		//cout << temp.size() << endl;
+		for (size_t i = 0; i < data.size(); i++)
+		{
+			cout << (char)(isalpha(data[i]) ? temp[data[i] - 'a'] : data[i]);
+		}
+		cout << endl;
+	}
+}
